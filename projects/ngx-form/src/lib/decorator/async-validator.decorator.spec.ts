1822535d8dc5d8467fbd42ef463653e301c52d0d
Oscar GUERIN <oscar.guerin@witty-services.fr>
2020-12-15 17:20:41 +0100

    test(validators): async validator tests

---
+++ projects/ngx-form/src/lib/decorator/async-validator.decorator.spec.ts
@@ -0,0 +1,44 @@
+import 'reflect-metadata';
+import {AsyncValidatorFn} from '@angular/forms';
+import {ASYNC_VALIDATORS_METADATA_KEY, AsyncValidator} from './async-validator.decorator';
+import {of} from 'rxjs';
+import {delay, map} from 'rxjs/operators';
+
+const myAsyncValidator: AsyncValidatorFn = () =>
+  of(void 0).pipe(
+    delay(10),
+    map(() => ({asyncError: true}))
+  );
+
+@AsyncValidator(myAsyncValidator)
+class LibraryForm {
+
+  public name: string = 'My Library';
+
+  @AsyncValidator([])
+  public numberOfBooks: number = 10;
+}
+
+class NotAForm {
+}
+
+describe('ValidatorDecorator', () => {
+
+  const form: LibraryForm = new LibraryForm();
+
+  it('should add async validator metadata on property with decorator', () => {
+    expect(Reflect.getMetadata(`${ASYNC_VALIDATORS_METADATA_KEY}:numberOfBooks`, form)).toBeDefined();
+  })
+
+  it('should not add async validator metadata on property without decorator', () => {
+    expect(Reflect.getMetadata(`${ASYNC_VALIDATORS_METADATA_KEY}:name`, form)).toBeUndefined();
+  })
+
+  it('should add async validator metadata on class with decorator', () => {
+    expect(Reflect.getMetadata(`${ASYNC_VALIDATORS_METADATA_KEY}`, LibraryForm)).toBeDefined();
+  })
+
+  it('should not add async validator metadata on class without decorator', () => {
+    expect(Reflect.getMetadata(`${ASYNC_VALIDATORS_METADATA_KEY}`, NotAForm)).toBeUndefined();
+  })
+})
