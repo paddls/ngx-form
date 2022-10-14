378a4baeb95386d4826ebe55412b5a0ea5e50e88
Oscar GUERIN <oscar.guerin@gmail.com>
2022-10-14 11:56:54 +0200

    release: v4.1.0-rc.2

--- projects/ngx-form/src/public-api.ts
+++ projects/ngx-form/src/public-api.ts
@@ -1,11 +1,15 @@
-export {AsyncValidator} from './lib/decorator/async-validator.decorator';
+export {AsyncValidator, AsyncValidatorConfig} from './lib/decorator/async-validator.decorator';
 export {BuildForm} from './lib/decorator/build-form.decorator';
+export {DisableOnSimpleConfig, DisableOnConfigFactoryFn, DisableOnConfigWithProviders, DisableOnConfig, DisableOnOptions, DisableOnContext, DisableOn} from './lib/decorator/disable-on.decorator';
 export {FormArray, FormArrayContext} from './lib/decorator/form-array.decorator';
 export {FormControl} from './lib/decorator/form-control.decorator';
 export {FormGroup, FormGroupContext} from './lib/decorator/form-group.decorator';
 export {FormChild, FormChildContext} from './lib/decorator/form-child.decorator';
 export {UpdateOn} from './lib/decorator/update-on.decorator';
-export {Validator} from './lib/decorator/validator.decorator';
+export {Validator, ValidatorConfig} from './lib/decorator/validator.decorator';
+export {AsyncValidatorFactory, AsyncValidatorFactoryFn, AsyncValidatorFactoryWithProviders} from './lib/factory/async-validator.factory';
+export {ValidatorFactory, ValidatorFactoryFn, ValidatorFactoryWithProviders} from './lib/factory/validator.factory';
+export {DisableOnFactory} from './lib/factory/disable-on.factory';
 
 export {NgxFormArray} from './lib/model/ngx-form-array.model';
 export {NgxFormControl} from './lib/model/ngx-form-control.model';
